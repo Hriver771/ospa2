f1fa6adb64d32775d66b6f34c5d712d8a71b1698
maplbop135 <48009213+maplbop135@users.noreply.github.com>
2019-04-29 21:42:49 +0900

    Add files via upload

---
+++ GameMap.c
@@ -0,0 +1,74 @@
+// 21500344 ¼­Âù¼Ö
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct _City{
+  int num;
+  int neighbor;
+  int *listN;
+}City;
+
+int myDP(City *c, int num, int i);
+
+#if 1
+
+int main(int argc, char* argv[]){
+
+  City *c;
+  int num, link, buf = 0, first, second, max = 0;
+
+  scanf("%d %d", &num, &link);
+
+  c = (City*)malloc(sizeof(City)*num);
+
+  for(int i=0; i < num; i++){
+    c[i].num = i;
+    c[i].neighbor = 0;
+    c[i].listN = (int*)malloc(sizeof(int)*(c[i].neighbor));
+  }
+  for(int i=0; i < link; i++){
+    scanf("%d %d", &first, &second);
+    c[first].neighbor++;
+    c[first].listN = (int*)realloc(c[first].listN, sizeof(int)*c[first].neighbor);
+    c[first].listN[ (c[first].neighbor) - 1 ] = second;
+    c[second].neighbor++;
+    c[second].listN = (int*)realloc(c[second].listN, sizeof(int)*c[second].neighbor);
+    c[second].listN[ (c[second].neighbor) - 1 ] = first;
+  }
+
+  for(int i=0; i < num; i++){
+    buf = myDP(c, num, i);
+    if(buf>max) max = buf;
+  }
+
+  printf("%d", max);
+
+  free(c);
+  return 0;
+}
+
+#endif
+
+int myDP(City *c, int num, int i){
+
+  int tmp = 0, min = i, cnt = 1, miso = i;
+
+  while(1){
+    for(int j = 0; j < c[miso].neighbor; j++){
+      if(c[miso].neighbor < c[ c[miso].listN[j] ].neighbor){
+        tmp = c[miso].listN[j];
+        if(min == miso) min = tmp;
+        else if(c[tmp].neighbor < c[min].neighbor) min = tmp;
+      }
+    }
+
+    if(min == miso){
+      return cnt;
+    } else{
+      cnt++;
+      miso = min;
+    }
+  }
+}
